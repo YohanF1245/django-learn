b98fe9552bf2129cb169e2cd8774ee1784b6fdcd
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-07-06 13:32:14 +0200

    feat(views.py): implement voting logic in vote function

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-from.models import Question
+from django.db.models import F
+from.models import Question, Choice
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -18,4 +20,20 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                question: question,
+                "error_message": "You didn't select a choice;",
+
+            },
+        )
+    else: 
+        selected_choice.votes = F("votes") + 1;
+        selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
